ac618b1c00f53ed9a4e6bddb8554c6d950af5651
田村克彦 <k-tamura@p1308.office.mapion.co.jp>
2018-05-25 18:06:50 +0900

    adding files

---
+++ BasicNotesEntity+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  BasicNotesEntity+CoreDataClass.swift
+//  ChordMaster
+//
+//  Created by 田村 克彦 on 2018/05/23.
+//  Copyright © 2018年 田村 克彦. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+public class BasicNotesEntity: NSManagedObject {
+
+}
---
+++ ChordMaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChordMaster
+//
+//  Created by 田村 克彦 on 2018/05/23.
+//  Copyright © 2018年 田村 克彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
