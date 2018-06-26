0a68921fd9367ddd28b782099d8b6efdd0e5808c
田村克彦 <k-tamura@p1308.office.mapion.co.jp>
2018-06-26 17:26:14 +0900

    grudge

--- ChordMaster/ViewController.swift
+++ ChordMaster/ViewController.swift
@@ -21,6 +21,31 @@ class ViewController: UIViewController {
         
         let appdelegate = UIApplication.shared.delegate as! AppDelegate
         let managedObjectContext = appdelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName:"BasicNote", in: managedObjectContext)
+        // データ追加
+        var chordBase = ChordBase(entity:entity!,insertInto:managedObjectContext)
+        chordBase.intvls = [0,3,6]
+        chordBase.kName = "メジャー"
+        chordBase.name = "major"
+        chordBase.symbol = ""
+        chordBase.symbol2 = "maj"
+        do{
+            try managedObjectContext.save()
+        }catch{
+            
+        }
+        chordBase = ChordBase(entity:entity!,insertInto:managedObjectContext)
+        chordBase.intvls = [0,3,6]
+        chordBase.kName = "マイナー"
+        chordBase.name = "minor"
+        chordBase.symbol = "m"
+        chordBase.symbol2 = "min"
+        do{
+            try managedObjectContext.save()
+        }catch{
+            
+        }
+        
         
         
     }
